f7aa805d37909784ed6e15f074ad754d2054dded
Michael Jard <mjard@misadventuregames.com>
2013-04-18 03:55:31 -0700

    Session Login
    + Frame of network Session object

--- gamelib/network.py
+++ gamelib/network.py
@@ -1,3 +1,56 @@
+import urlparse
 import requests
 
-Session = requests.Session
+import pyglet
+
+urls = {
+	"login": "register/",
+}
+
+class Session(pyglet.event.EventDispatcher):
+	def __init__(self, server = 'http://localhost:8000'):
+		self.server = server
+		self.http_session = None
+
+	def login(self, user, password):
+		session = requests.Session()
+
+		url = urlparse.urljoin(self.server, urls['login'])
+
+		try:
+			resp = session.post(url, data={	'user': user,
+											'password': password})
+		except requests.exceptions.RequestException:
+			self.http_session = None
+			self.dispatch_event('on_no_connection')
+			return
+
+		if resp.status_code == requests.codes.ok:
+			self.http_session = session
+			self.dispatch_event('on_connected')
+			return
+
+		self.dispatch_event('on_login_failure', resp.text)
+
+
+	def is_logged_in(self):
+		return self.http_session != None
+
+	def dungeons(self):
+		pass
+
+	def get_dungeon(self, dungeon_id):
+		pass
+
+	def upload_dungeon(self, dugeon):
+		pass
+
+	def get_replay(self, replay_id):
+		pass
+
+	def upload_replay(self, replay):
+		pass
+
+Session.register_event_type('on_connected')
+Session.register_event_type('on_no_connection')
+Session.register_event_type('on_login_failure')
