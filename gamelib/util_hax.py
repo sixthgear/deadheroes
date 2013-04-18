5c9e450df4f2e681e03c4ea9d20ee30500c3558b
Michael Jard <mjard@misadventuregames.com>
2013-04-18 14:10:11 -0700

    What a Blunder
    hax fell flat

--- gamelib/util_hax.py
+++ gamelib/util_hax.py
@@ -4,4 +4,4 @@ def defer(func, *args, **kwargs):
     """
     I'm almost sorry.
     """
-    pyglet.clock.schedule_once(lambda dt: func(*args, **kwargs))
+    pyglet.clock.schedule_once(lambda dt: func(*args, **kwargs), 0.0)
