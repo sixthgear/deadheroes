72cfb00038fc8611be9821af7bb1bb5a2807fc67
sixthgear <sixthgear@sixthgear.ca>
2013-04-20 23:10:00 -0700

    more work on menu screen

--- gamelib/controller/menu.py
+++ gamelib/controller/menu.py
@@ -9,14 +9,14 @@ from gamelib.objects.obj import Point
 
 class Menu(object):
 
-    per_page = 25
+    per_page = 12
 
     def __init__(self, window):
         self.window = window
         self.music = None
         self.keys = key.KeyStateHandler()
         self._label_batch = graphics.Batch()
-
+        self.player_dungeon_widget = None
         self.labels = {
 
         'heroes': text.Label(
@@ -31,19 +31,27 @@ class Menu(object):
             font_size=120, font_name='DYLOVASTUFF', anchor_x='left', anchor_y='top',
             color=(150, 100, 100, 50), batch=self._label_batch),
 
-        'wealth': text.Label(
-            '{}: {} EVIL DOLLARS'.format(self.window.player_data['name'], self.window.player_data['wealth']), 
-            x=64, y=768, 
-            font_size=12, font_name='DYLOVASTUFF', anchor_x='left', anchor_y='center', 
-            color=(100,100,100,255),
-            batch=self._label_batch),  
-
         'title': text.Label(
             'RAID A DUNGEON', 
             x=454, y=572, 
             font_size=24, font_name='DYLOVASTUFF', anchor_x='left', anchor_y='center',
             color=(100,100,100,255),
             batch=self._label_batch),
+
+        'yourdungeon': text.Label(
+            'YOUR DUNGEON (HIT TAB TO EDIT)', 
+            x=454, y=100, 
+            font_size=24, font_name='DYLOVASTUFF', anchor_x='left', anchor_y='center',
+            color=(100,100,100,255),
+            batch=self._label_batch),
+
+        # 'wealth': text.Label(
+        #     '{}: {} EVIL DOLLARS'.format(self.window.player_data['name'], self.window.player_data['wealth']), 
+        #     x=454, y=170, 
+        #     font_size=12, font_name='DYLOVASTUFF', anchor_x='left', anchor_y='center', 
+        #     color=(100,100,100,255),
+        #     batch=self._label_batch),  
+
         }
 
         self.header = DungeonListingWidget(
@@ -62,8 +70,23 @@ class Menu(object):
 
         self.dungeons = self.window.session.dungeons()
         self.dungeons_widgets = []
-
+        print self.dungeons
         for i in range(self.page_start, min(len(self.dungeons), self.page_start+self.per_page)):
+
+            if self.dungeons[i]['username'] == self.window.player_data['name']:
+                self.player_dungeon_widget = DungeonListingWidget(
+                    x=454,
+                    y=56,
+                    id=self.dungeons[i]['id'], 
+                    username=self.dungeons[i]['username'], 
+                    age=prettydate(int(self.dungeons[i]['age'])),
+                    value='${}'.format(self.dungeons[i]['value']),
+                    attempts=self.dungeons[i]['attempts'], 
+                    batch=self._label_batch
+                )
+                i -=1                
+                continue
+
 
             d = DungeonListingWidget(
                 x=454,
@@ -76,7 +99,7 @@ class Menu(object):
                 batch=self._label_batch
             )
             self.dungeons_widgets.append(d)
-            self.window.push_handlers(d)
+            # self.window.push_handlers(d)
 
     def update(self, dt):
         pass
@@ -103,7 +126,7 @@ class Menu(object):
             pyglet.app.exit()
 
     def on_mouse_motion(self, x, y, dx, dy):
-        for d in self.dungeons_widgets:
+        for d in self.dungeons_widgets + [self.player_dungeon_widget]:
             if collide.AABB_to_AABB(Point(x,y), 0, 0, Point(d.x, d.y), 800, 24):
                 for t in d.labels.values():
                     t.color=(0,0,0,255)
@@ -118,6 +141,12 @@ class Menu(object):
                 m = self.window.load(d.username, d.id)
                 defer(self.window.play, m)
                 # print dungeon
+        if self.player_dungeon_widget:
+            if collide.AABB_to_AABB(Point(x,y), 0, 0, Point(self.player_dungeon_widget.x, self.player_dungeon_widget.y), 800, 24):
+                m = self.window.load(self.window.player_data['name'])
+                defer(self.window.edit, m)
+
+
 
     def cleanup(self):
         for d in self.dungeons:
